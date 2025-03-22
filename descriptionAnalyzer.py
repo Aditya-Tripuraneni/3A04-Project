4e9b6c478c6223a1991aac83cccadc16fd635975
nasrallr <nasrallr@mcmaster.ca>
2025-03-22 01:38:34 -0400

    Implement DescriptionFinder and add Description class (close #9)

--- descriptionAnalyzer.py
+++ descriptionAnalyzer.py
@@ -1,15 +1,40 @@
 from agents import Agent
 from data import Data
+from descriptionFinder import DescriptionFinder
+from description import Description
 
 
 class DescriptionAnalyzer(Agent):
 
+    def __init__(self):
+        self.prediction = None
+
     def analyze_data(self, data: Data):
-        pass
+        description_finder = DescriptionFinder(data)
+        song_data = description_finder.identify_song()
+        self.prediction = song_data.get_song_data()
 
     def get_confidence_score(self):
-        return 0.5
+        return self.prediction["confidence_score"]
     
     def submit_partial_solution(self):
-        return 6
+        return self.prediction
+
+
+# Example usage
+sample_description = Description(
+    artist="Justin Timberlake",
+    genre="Pop / R&B",
+    year="2013",
+    albumName="The 20/20 Experience",
+    mood="Emotional, Romantic, Reflective",
+    genderOfArtist="Male",
+    language="English",
+    region="North America",
+    featuredArtist="None"
+)
+
+
+description_agent = DescriptionAnalyzer()
+description_agent.analyze_data(sample_description)
     
