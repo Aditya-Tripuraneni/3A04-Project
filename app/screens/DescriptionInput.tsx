0da23cf8751a1a8ca57a2c0623c3bb037f34a316
nasrallr <nasrallr@mcmaster.ca>
2025-03-25 18:25:50 -0400

    Integrate frontend and backend

--- app/screens/DescriptionInput.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function DescriptionInput() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Description Search Coming Soon!</Text>
-    </View>
-  );
-}
